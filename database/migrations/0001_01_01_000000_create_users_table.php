f4a4b7ea1cafaaf14f8e8f593a3756a3b7559261
FilipStraka <straka168@gmail.com>
2024-06-10 15:12:12 +0200

    Created ideas_table thru miartisan make:migration command, in database/migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); //created at, updated at
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
